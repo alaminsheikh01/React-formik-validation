3da3c2b2cc4feb10ff5e64fdcd3a0e59d2c09579
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-09-17 23:42:46 +0600

    added validate using formik

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,20 +1,51 @@
 import React from 'react'
 import {useFormik} from 'formik'
 
+const initialValues =  {
+    name:'',
+    email:'',
+    channel:''
+}
+
+const onSubmit = values =>{
+    console.log('Form data', values);
+}
+
+const validate = values=>{
+    // values.name values.email values.channel
+    // errors.name errors.email errors.channel
+    let errors ={}
+
+    if(!values.name){
+        errors.name = 'Required'
+    }
+
+    if(!values.email){
+        errors.email = 'Required'
+    }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+        errors.email = 'Invalid email format'
+    }
+
+    if(!values.channel){
+        errors.channel = 'Required'
+    }
+
+    return errors;
+}
+
+
 function YoutubeForm(){
 
     const formik = useFormik({
-        initialValues:{
-            name:'',
-            email:'',
-            channel:''
-        }
+        initialValues,
+        onSubmit,
+        validate
     })
-    console.log("form values", formik.values);
+    // console.log("form values", formik.values);
 
     return (
         <div>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
                 <label htmlFor='name'>Name</label>
                 <input type="text" id="name" name='name' onChange={formik.handleChange} value={formik.values.name}/>
 
