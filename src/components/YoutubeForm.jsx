76d978915f3fcaf2fd614229474b245924689993
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-09-19 00:55:30 +0600

    clean code

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -6,38 +6,41 @@ const initialValues = {
   name: "",
   email: "",
   channel: "",
+  comments: "",
+  address: "",
 };
 
-const onSubmit = (values) => {
-  console.log("Form data", values);
-};
+// const onSubmit = (values) => {
+//   console.log("Form data", values);
+// };
 
-const validate = (values) => {
-  // values.name values.email values.channel
-  // errors.name errors.email errors.channel
-  let errors = {};
+// const validate = (values) => {
+//   // values.name values.email values.channel
+//   // errors.name errors.email errors.channel
+//   let errors = {};
 
-  if (!values.name) {
-    errors.name = "Required";
-  }
+//   if (!values.name) {
+//     errors.name = "Required";
+//   }
 
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email format";
-  }
+//   if (!values.email) {
+//     errors.email = "Required";
+//   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+//     errors.email = "Invalid email format";
+//   }
 
-  if (!values.channel) {
-    errors.channel = "Required";
-  }
+//   if (!values.channel) {
+//     errors.channel = "Required";
+//   }
 
-  return errors;
-};
+//   return errors;
+// };
 
 const validationSchema = Yup.object({
   name: Yup.string().required("Required!"),
   email: Yup.string().email("Invalid email format").required("Required"),
   channel: Yup.string().required("Required"),
+  comments: Yup.string().required("Required"),
 });
 
 function YoutubeForm() {
@@ -59,57 +62,44 @@ function YoutubeForm() {
       <Form>
         <div className="form-control">
           <label htmlFor="name">Name</label>
-          <Field
-            type="text"
-            id="name"
-            name="name"
-            // onBlur={formik.handleBlur}
-            // onChange={formik.handleChange}
-            // value={formik.values.name}
+          <Field type="text" id="name" name="name" />
 
-            // {...formik.getFieldProps("name")}
-          />
-          {/* {formik.touched.name && formik.errors.name ? (
-            <div className="error">{formik.errors.name}</div>
-          ) : null} */}
-          <ErrorMessage name="name" />
+          <ErrorMessage name="name" component="div" />
         </div>
 
         <div className="form-control">
           <label htmlFor="email">E-mail</label>
-          <Field
-            type="email"
-            id="id"
-            name="email"
-            // onBlur={formik.handleBlur}
-            // onChange={formik.handleChange}
-            // value={formik.values.email}
-
-            // {...formik.getFieldProps("email")}
-          />
-          {/* {formik.touched.email && formik.errors.email ? (
-            <div className="error">{formik.errors.email}</div>
-          ) : null} */}
-          <ErrorMessage name="email" />
+          <Field type="email" id="id" name="email" />
+          <ErrorMessage name="email" component="div" />
         </div>
 
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
-          <Field
-            type="text"
-            id="channel"
-            name="channel"
-            // onBlur={formik.handleBlur}
-            // onChange={formik.handleChange}
-            // value={formik.values.channel}
-            // if we use just a line then it will be happen same as before.
-            // {...formik.getFieldProps("channel")}
-          />
-          {/* {formik.touched.channel && formik.errors.channel ? (
-            <div className="error">{formik.errors.channel}</div>
-          ) : null} */}
-          <ErrorMessage name="channel" />
+          <Field type="text" id="channel" name="channel" />
+          <ErrorMessage name="channel" component="div" />
         </div>
+        <div className="form-control">
+          <label htmlFor="comments">Comments</label>
+          <Field as="textarea" id="comments" name="comments" />
+          <ErrorMessage name="comments" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="address">Address</label>
+          <Field name="address">
+            {(props) => {
+              const { field, form, meta } = props;
+              console.log("Render props", props);
+              return (
+                <div>
+                  <input type="text" id="address" {...field} />
+                  {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+                </div>
+              );
+            }}
+          </Field>
+        </div>
+
         <button type="submit">Submit</button>
       </Form>
     </Formik>
