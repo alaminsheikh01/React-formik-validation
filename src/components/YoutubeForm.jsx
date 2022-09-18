aafa7a82361e63f1765838cc0d0b2b8ff76b3c86
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-09-18 11:33:54 +0600

    error msg show in user side

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -46,15 +46,24 @@ function YoutubeForm(){
     return (
         <div>
             <form onSubmit={formik.handleSubmit}>
+
+                <div className='form-control'>
                 <label htmlFor='name'>Name</label>
                 <input type="text" id="name" name='name' onChange={formik.handleChange} value={formik.values.name}/>
+                {formik.errors.name? <div className='error'>{formik.errors.name}</div>:null}
+                </div>
 
+                <div className='form-control'>
                 <label htmlFor='email'>E-mail</label>
                 <input type="email" id="id" name="email" onChange={formik.handleChange} value={formik.values.email}/>
-
+                {formik.errors.email? <div className='error'>{formik.errors.email}</div>:null}
+                </div>
+                
+                <div className='form-control'>
                 <label htmlFor='channel'>Channel</label>
                 <input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel}/>
-
+                {formik.errors.channel? <div className='error'>{formik.errors.channel}</div>:null}
+                </div>
                 <button type="submit">Submit</button>
             </form>
         </div>
